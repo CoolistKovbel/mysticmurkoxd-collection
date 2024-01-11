ad083dd3eecaafa67ce1c55f3d9c16ed442f7328
coolistkovbel <coolistkovbel@gmail.com>
2024-01-11 05:59:40 -0500

    still basic... going to be working on the other pages.. dont know if i am going to finish might take a nap now cause i feel like poop after crying

--- app/(dApp)/about/page.tsx
+++ app/(dApp)/about/page.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="min-h-screen flex justify-center  w-full">
 
-export default AboutPage
+      <div className="w-[350px] md:w-[1100px] flex items-center flex-col">
+
+        <h2 className="text-2xl md:text-5xl font-bold self-start my-10 p-4">
+          About This Collection
+        </h2>
+
+        <div className="flex flex-col gap-10 w-[80%]">
+          <p className="leading-7 text-[12px] md:text-[18px]">
+            This collection was something I had thought about a while ago. My
+            cat had a tragic accident one day that costed about 400-500 to just
+            kill him or what my sister would say, put him down. It wasnt
+            something easy the docters could fix and we didnt have enough money
+            to genuelly help him. I didnt want to put him down but he was
+            already getting old, i didnt think it was best to do, i told him
+            that when he comes home he could have all the shrimp he could eat
+            but on that very day we litterally just i dont know feel like we
+            murdered him.
+          </p>
+
+          <p className="leading-7 text-[12px] md:text-[18px]">
+            So i ended up scratching my project that i wanted to do and wanted
+            to do something fun that I could always cherish myself. He was such
+            an awesome cat, had him ever since I was 7. We fought when we were
+            kids but as time passed we learned to love each other. He eventually
+            started to listen to me and I would know when he wanted to leave my
+            room. He got used to belly rubs he was best friend. My little
+            munchkin.
+          </p>
+
+          <p className="leading-7 text-[12px] md:text-[18px]">
+            I forgot what the technical terms are but his legs basically got
+            paralized and he was move with his front 2 paws instead. It was the
+            worse moment of my life because I myself did not want to belive that
+            that day would be the last day that i would ever see him.
+          </p>
+
+          <p className="leading-7 text-[12px] md:text-[18px]">
+            So I wanted to created this project to just have something to
+            remember him by at the same time spread some awareness to animal
+            hospitals to be able to study and advance their veterinarian skills
+            towards pets to be able to fix them just as they would with humans.
+            Im not sure if its a long way to go but I am really sick and tired
+            of having people die over the silliest things or even having my pets
+            die. I understand we are all mammels and have some sort of life
+            expectancy but if we are trying to make a change and go to mars we
+            need to focus on human health and if possible pet health as well.
+            But not turn this into some sort of owaises where the reach get
+            treated with better care than the poor because there is some sort of
+            emergency.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default AboutPage;
