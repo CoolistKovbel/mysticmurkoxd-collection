2eb76f610ebfacad679251479ce28a37b4f37528
coolistkovbel <coolistkovbel@gmail.com>
2024-01-15 06:27:21 -0500

    why no work!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ModalProvider } from '@/components/providers/modal-provider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'NFT Dapp',
-  description: 'A small NFT collection compatabile with this website to allow you to message your friends create a community or announce something...',
-}
+  title: "NFT Dapp",
+  description:
+    "A small NFT collection compatabile with this website to allow you to message your friends create a community or announce something...",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
-          <ModalProvider />
-          {children}
-
+        <ModalProvider />
+        {children}
       </body>
     </html>
-  )
+  );
 }
